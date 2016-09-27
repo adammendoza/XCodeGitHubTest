d6a63c2b4e54ad2f8a17a9989e0ceff248b2f76e
Adam J. Mendoza <adammendoza@hotmail.com>
2016-09-26 23:09:02 -0700

    Test Results. Xcode fails to pull or push after connecting remote to local repo. SourceTree succeeded.

--- XCodeGitHubTest/ViewController.swift
+++ XCodeGitHubTest/ViewController.swift
@@ -13,7 +13,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
         // Test Change from XCode
+        
+        // Created a new Project with git support
+        // Created the repo on GitHub
+        // Added the remote via Xcode
+        // Made a commit
+        
+        // Test if I can push with XCode
+        
+        // XCode Failed to do a pull or a push but it worked with SourceTree
     }
 
     override func didReceiveMemoryWarning() {
